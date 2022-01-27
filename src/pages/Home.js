9eda6e631b46d08e04ca697263ecb5671c880f28
Paulo Vitor <paulovitor2123@gmail.com>
2022-01-26 23:57:47 -0300

    Add an onscreen greeting depending on the weather

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     View,
     Text,
@@ -13,14 +13,29 @@ import SkillCard from '../components/SkillCard';
 export function Home() {
     const [newSkill, setNewSkill] = useState('');
     const [mySkills, setMySkills] = useState([]);
-
+    const [greeting, setGreeting] = useState('');
     const handleNewAddSkill = () => {
         setMySkills(oldState => [...oldState, newSkill]);
     }
 
+    useEffect(() => {
+        const currentHour = new Date().getHours();
+
+        if (currentHour < 12) {
+            setGreeting('Good morning');
+        } else if (currentHour >= 12 && currentHour < 18) {
+            setGreeting('Good afternoon');
+        } else {
+            setGreeting('Good night');
+        }
+    }, [greeting])
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Welcome, Paulo</Text>
+            <Text style={styles.greetings}>
+                {greeting}
+            </Text>
             <TextInput
                 style={styles.input}
                 placeholder='New skill'
@@ -65,5 +80,8 @@ const styles = StyleSheet.create({
         padding: Platform.OS === 'ios' ? 15 : 10,
         marginTop: 30,
         borderRadius: 7
+    },
+    greetings: {
+        color: '#FFF'
     }
 })
